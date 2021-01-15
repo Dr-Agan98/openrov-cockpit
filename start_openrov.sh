818f8e4b2a5f6c88677cee31486d5d1a0687594c
Dr-Agan98 <dragan.vule98@gmail.com>
2021-01-15 18:53:52 +0100

    Refactored the launch script

--- start_openrov.sh
+++ start_openrov.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-NODE_ENV=development PLATFORM=mock BOARD=mock3000 DEV_MODE=true USE_MOCK=true CPU_MOCK=123MOCK MOCK_VIDEO_TYPE=MJPEG MOCK_VIDEO_HARDWARE=true DEBUG="bridge, mcu, cpu, *:Notifications, app:mjpeg*" port=8080 configfile='/tmp/rovconfig.json' env plugins__ui-manager__selectedUI='new-ui' pluginsDownloadDirectory='/tmp/plugins' cacheDirectory='/tmp/cache' DATADIR='/tmp' IGNORE_CACHE=true LOG_LEVEL='debug' node --nolazy `dirname "$0"`/src/cockpit.js
+NODE_ENV=development \
+PLATFORM=mock \
+BOARD=mock3000 \
+DEV_MODE=true \
+USE_MOCK=true \
+CPU_MOCK=123MOCK \
+MOCK_VIDEO_TYPE=MJPEG \
+MOCK_VIDEO_HARDWARE=true \
+DEBUG="bridge, mcu, cpu, *:Notifications, app:mjpeg*" \
+port=8080 configfile='/tmp/rovconfig.json' \
+env plugins__ui-manager__selectedUI='new-ui' \
+pluginsDownloadDirectory='/tmp/plugins' \
+cacheDirectory='/tmp/cache' DATADIR='/tmp' \
+IGNORE_CACHE=true LOG_LEVEL='debug' \
+node --nolazy `dirname "$0"`/src/cockpit.js
